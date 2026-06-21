985ab652658ab913e146634fea171249ea3c062b
FabricioS2 <fabricio.g@escolar.ifrn.edu.br>
2026-06-20 23:31:47 -0300

    retirando codigo comentado

--- user_service/serializers.py
+++ user_service/serializers.py
@@ -3,49 +3,6 @@ from rest_framework import serializers
 from .models import Usuario, Perfil
 from .validators import validar_cpf   
 
-# class UserRegistrationSerializer(serializers.ModelSerializer):
-#     # campos do Perfil que virão na requisição
-#     cpf = serializers.CharField(max_length=14, validators=[validar_cpf])
-#     telefone = serializers.CharField(max_length=15)
-#     tipo_usuario = serializers.ChoiceField(choices=Perfil.TIPO_CHOICES)
-#     foto = serializers.ImageField(required=False)
-
-#     password = serializers.CharField(write_only=True, min_length=6)
-
-#     class Meta:
-#         model = Usuario
-#         fields = ['email', 'nome', 'password', 'cpf', 'telefone', 'tipo_usuario', 'foto']
-
-#     def validate_email(self, value):
-#         if Usuario.objects.filter(email=value).exists():
-#             raise serializers.ValidationError("Este e-mail já está cadastrado.")
-#         return value
-
-#     def create(self, validated_data):
-#         # extrai os campos do Perfil
-#         perfil_data = {
-#             'cpf': validated_data.pop('cpf'),
-#             'telefone': validated_data.pop('telefone'),
-#             'tipo_usuario': validated_data.pop('tipo_usuario'),
-#             'foto': validated_data.pop('foto', None),
-#         }
-#         password = validated_data.pop('password')
-
-#         # cria o usuário
-#         user = Usuario(**validated_data)
-#         user.set_password(password)
-#         user.save()
-
-#         # define is_motorista automaticamente
-#         perfil_data['is_motorista'] = (perfil_data['tipo_usuario'] == 'Motorista')
-
-#         # cria o perfil vinculado
-#         Perfil.objects.create(usuario=user, **perfil_data)
-
-#         return user
-
-# serializers.py
-# serializers.py
 class UserRegistrationSerializer(serializers.ModelSerializer):
     cpf = serializers.CharField(
         max_length=14, validators=[validar_cpf], write_only=True
