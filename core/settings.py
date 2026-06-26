8b5227ecb880edbe5179e0e4ad271bb798ee4796
FabricioS2 <fabricio.g@escolar.ifrn.edu.br>
2026-06-26 15:16:00 -0300

    integrando postgress

--- core/settings.py
+++ core/settings.py
@@ -99,10 +99,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'raijmobi_user_db'),
+        'USER': os.environ.get('DB_USER', 'raijmobi_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'raijmobi_pass'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
